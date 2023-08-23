7b5b92203350eb0b628c9e65e7cfd1e9f6bff5a5
DanishAbbas <danish.abbas@arbisoft.com>
2023-08-23 13:44:27 +0500

    Worked on reports part of the program

--- report_generator.py
+++ report_generator.py
@@ -1,6 +1,3 @@
-import datetime
-import pandas as pd
-
 
 def weather_in_year_report(highest_temp_data, highest_humidity_data, lowest_temp_data, lowest_humidity_data):
     # Getting information for the highest temperature
@@ -27,9 +24,47 @@ def weather_in_year_report(highest_temp_data, highest_humidity_data, lowest_temp
             )
 
 
-def weather_in_month_report(avg_highest_temperature_value, avg_lowest_temperature_value, avg_mean_humidity_value):
-    pass
+def weather_in_month_report(avg_highest_temperature_value, avg_lowest_temperature_value,
+                            avg_mean_humidity_value):  # Generating report for month
+
+    return (f"Highest Average Temperature: {avg_highest_temperature_value}C\n"
+            f"Lowest Average Temperature : {avg_lowest_temperature_value}C\n"
+            f"Average Mean Humidity : {avg_mean_humidity_value}%"
+            )
 
 
-def weather_each_day_report():
-    pass
+def weather_each_day_report(temp_data_each_day):
+    days = []
+    for index, day in temp_data_each_day.iterrows():
+        day_max = round(day['Max TemperatureC'])
+        day_max_date = day['PKT']
+        day_min = round(day['Min TemperatureC'])
+        day_min_date = day['PKT']
+        days.append(bar_charts(day_max, day_max_date, day_min, day_min_date))
+
+    return days
+
+
+def bar_charts(day_max, day_max_date, day_min, day_min_date):
+    # ANSI escape codes for text colors
+    RED = "\033[91m"
+    BLUE = "\033[94m"
+    RESET = "\033[0m"
+
+    chart1 = ''
+    chart2 = ''
+    for x in range(day_max):
+        chart1 += '+'
+    if day_min >= 0:
+        for y in range(day_min):
+            chart2 += '+'
+    else:
+        for y in range(day_min):
+            chart2 += '-'
+
+    day_max_info = RED + f"{day_max_date}  {chart1}  {day_max}C" + RESET
+    day_min_info = BLUE + f"{day_min_date}  {chart2}  {day_min}C" + RESET
+
+    return (f"{day_max_info}\n"
+            f"{day_min_info}"
+            )
--- weather_calculations.py
+++ weather_calculations.py
@@ -6,6 +6,8 @@ weather_data = main_conversion()
 
 def weather_in_year(date):
     filtered_data = weather_data[weather_data['PKT'].dt.year == int(date)]  # Getting data for the year specified
+
+    # formatting datetime
     filtered_data.loc[:, 'PKT'] = filtered_data['PKT'].dt.strftime('%d %B')
 
     # Getting highest temperature and humidity values.
@@ -27,35 +29,45 @@ def weather_in_month(date):
     filtered_data = weather_data[(weather_data['PKT'].dt.year == year) & (weather_data['PKT'].dt.month == month)]
 
     # Finding the average highest temperature
-    avg_highest_temp = average(filtered_data, 'Max TemperatureC')
+    avg_highest_temp = round(average(filtered_data, 'Max TemperatureC'))
 
     # Finding the lowest average temperature
-    avg_lowest_temp = average(filtered_data, 'Min TemperatureC')
+    avg_lowest_temp = round(average(filtered_data, 'Min TemperatureC'))
 
     # Finding Average humidity
-    avg_humidity = average(filtered_data, 'Mean Humidity')
+    avg_humidity = round(average(filtered_data, 'Mean Humidity'))
 
+    # Generating report
     report = weather_in_month_report(avg_highest_temp, avg_lowest_temp, avg_humidity)
 
     print(report)
 
 
 def weather_each_day(date):
-    year, month = map(int, date.split('/'))  # filtering the data for the required values
+    # filtering the data for the required values
+    year, month = map(int, date.split('/'))
     filtered_data = weather_data[(weather_data['PKT'].dt.year == year) & (weather_data['PKT'].dt.month == month)]
     filtered_data = filtered_data[['PKT', 'Max TemperatureC', 'Min TemperatureC']]
 
+    # formatting datetime
+    filtered_data.loc[:, 'PKT'] = filtered_data['PKT'].dt.strftime('%d %B')
+
+    # Generating report
     report = weather_each_day_report(filtered_data)
 
+    # Printing report for each day
+    for each_day in report:
+        print(each_day)
 
-def highest_weather_value(filtered_data, search_field):
+
+def highest_weather_value(filtered_data, search_field):  # This function finds the highest value
     max_idx = filtered_data[search_field].idxmax()
     max_value_row = filtered_data.loc[max_idx]
 
     return max_value_row
 
 
-def lowest_weather_value(filtered_data, search_field):
+def lowest_weather_value(filtered_data, search_field):  # This function finds the lowest value
     min_idx = filtered_data[search_field].idxmin()
     min_value_row = filtered_data.loc[min_idx]
 
@@ -67,4 +79,4 @@ def average(filtered_data, search_field):
     return average_value
 
 
-weather_in_year('2008')
+weather_each_day('2008/7')
